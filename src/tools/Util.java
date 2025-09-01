5ef7591d3ff109c80ab17adb8fa42559db4d08b2
thaisMoraes13 <thais.oliveira5@estudante.ifms.edu.br>
2025-09-01 07:45:55 -0400

    aula que criamos o habilitar.

---
+++ src/tools/Util.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tools;
+
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author u07431666128
+ */
+public class Util {
+    public static void habilitar(boolean valor, JComponent ... componentes) {
+        for (int i = 0; i < componentes.length; i++) {
+            componentes[i].setEnabled(valor);
+            
+        }
+        
+    }
+}
